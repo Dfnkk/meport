273f7032a7209085b17bc314cc73c83229d4a483
Nikhil <60377182+Dfnkk@users.noreply.github.com>
2020-10-13 14:46:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const buttonForm = document.getElementById('submit-btn');
 
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function() {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + "%";
+};
+
 buttonForm.onclick = () => {
   const nameVal = document.getElementById('name').value;
   const subVal = document.getElementById('sub').value;
